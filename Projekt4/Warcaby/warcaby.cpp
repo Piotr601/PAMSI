192e3af7df684e00b1b2426474364ea9a1372ced
Piotr <piotr.niedziolka601@gmail.com>
2020-05-23 20:36:23 +0200

    testy

---
+++ Projekt4/Warcaby/warcaby.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Szachownica {
+    int Szach[8][8];
+public:
+    Szachownica()
+    {
+        for (int y = 0; y < 8; y++)
+        {
+            for (int x = 0; x < 8; x++)
+            {
+                Szach[x][y] = 0;
+            }
+        }
+
+        Pionki();
+    }
+
+    void Wyświetl()
+    {
+        for (int y = 0; y < 8; y++)
+        {
+            cout << " +---+---+---+---+---+---+---+---+" << endl;
+            for (int x = 0; x < 8; x++)
+            {
+                cout << " | " << Szach[x][y];
+            }
+            cout << " | " << endl;
+        }
+        cout << " +---+---+---+---+---+---+---+---+" << endl;
+    }
+
+    void Pionki()
+    {
+        for (int y = 0; y < 3; y++)
+        {
+            for (int x = 0; x < 8; x++)
+            {
+               Szach[x][y] = 1;
+            }
+        }
+
+        for (int y = 5; y < 8; y++)
+        {
+            for (int x = 0; x < 8; x++)
+            {
+                Szach[x][y] = 2;
+            }
+        }
+    }
+};
+
+int main()
+{
+    Szachownica szach;
+
+    szach.Wyświetl();
+}
